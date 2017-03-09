e91cc2c86682c26f6112d58fa2eae857af87bf83
Jair Huaman Canqui <jairfrancesco@gmail.com>
2017-03-09 05:20:36 -0500

    Create a Look model and add Route

---
+++ server/api/look/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var controller = require('./look.controller')
+var express = require('express');
+var router = express.Router();
+
+var auth = require('../../auth/auht.service');
+
+
+module.exports = router;
